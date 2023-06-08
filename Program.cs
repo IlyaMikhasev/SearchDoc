769959431559e394bbd5364bdae7fff3d542d08b
илья михасев <psfxslon@gmail.com>
2023-06-08 20:02:43 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchDoc
+{
+    internal class Program
+    {
+        static (string[] arr, DateTime date) InputValidation() {
+            string path = string.Empty;
+            string[] strFile = new string[30];
+            DateTime dateStart=DateTime.Now;
+            bool correctIn = false;
+            do
+            {
+                correctIn = false;
+                Console.WriteLine("Введите дату, с которой начинать поиск в формате ГГГГ.ММ.ДД");
+                try
+                {
+                    string dateInput = Console.ReadLine();
+                    var tmp = dateInput.Split(',', '.', '\\', '/');
+                    dateStart = new DateTime(int.Parse(tmp[0]), int.Parse(tmp[1]), int.Parse(tmp[2]));
+                    correctIn = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Неккоректный ввод даты");
+                }
+            } while (!correctIn);
+            do
+            {
+                correctIn = false;
+                Console.WriteLine("Введите путь к файлу с индификаторами");
+                try
+                {
+                    path = Console.ReadLine();
+                    StreamReader reader = new StreamReader(path);
+                    strFile = reader.ReadToEnd().Split(' ', '\r', '\n', '\0');
+                    correctIn = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Неккоректный путь к файлу");
+                }
+            } while (!correctIn);
+            return (strFile, dateStart);
+
+        }
+        static void Main(string[] args)
+        {
+            SearchFile searchFile = new SearchFile(InputValidation());
+            string pathFile = searchFile.SearchFilePath();
+            if (pathFile != string.Empty)
+                Console.WriteLine($"путь к вашему файлу {pathFile}");
+            else
+                Console.WriteLine("файл не найден");
+        }
+    }
+}
---
+++ SearchFile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchDoc
+{
+    internal class SearchFile
+    {
+        private string[] _arrIndification=new string[30];
+        private DateTime _dateStartSearch;
+
+        public SearchFile((string[] arr, DateTime date) values) {
+            _arrIndification = values.arr;
+            _dateStartSearch = values.date;
+        }
+        public string SearchFilePath() {
+            string path=string.Empty;
+            string myDoc = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string myDesctop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string applData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string[] folders = { myDoc, myDesctop, applData };
+            foreach (var item in folders)
+            {
+                try
+                {
+                    foreach (var filename in Directory.EnumerateFiles(item,"*.txt", SearchOption.AllDirectories)) {
+                        StreamReader streamReader = new StreamReader(filename);
+                        FileInfo fileInfo = new FileInfo(filename);
+                        DateTime timeOfModify = fileInfo.LastWriteTime;
+                        DateTime timeOfСreate = fileInfo.CreationTime;
+                        if (timeOfСreate >= _dateStartSearch | timeOfModify >= _dateStartSearch) {
+                            foreach (var key in _arrIndification) {
+                                if (filename.Contains(key))
+                                    return filename;                        
+                            }                    
+                        }                    
+                    }                
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine( $"Ошибка: {ex.Message}");
+                }
+            }
+            return path;
+        }
+    }
+}
